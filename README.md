a977296a34e7e5f828e15802813f51feb713bae9
jeff-maxwell <jeff@tcnsolutions.com>
2019-03-05 21:15:56 -0600

    Add Jungle Scene and jump script

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Team Asteroids Scroller Game
 
 This is the Unity game project for team asteroids.
+
+## Asset Store
+Desert - Free Parallax Desert Background - by Cryptogene
+Desert - Desert Kits 64 Sample - by 256PX
+Jungle Assets - 2D Jungle Collection - Free - by Skipan
---
+++ sourceCode/Scroller/Assets/Scripts/Jump.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float xVelocity = 0.5f;
+    public float yVelocity = 10.0f;
+
+    private Rigidbody2D rigidBody;
+    private bool facingRight = true;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        float move = Input.GetAxis("Horizontal");
+
+        if (move != 0)
+        {
+            if (move > 0 && !facingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && facingRight)
+            {
+                Flip();
+            }
+
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f);
+            rigidBody.AddForce(new Vector2(xVelocity, yVelocity), ForceMode2D.Impulse);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        xVelocity = xVelocity * -1.0f;
+    }
+}
