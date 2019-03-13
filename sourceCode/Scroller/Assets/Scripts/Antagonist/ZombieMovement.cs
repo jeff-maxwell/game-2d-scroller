5ab8a62baec2f91578eb6ad19b9637fe54f2d1a4
Centupli <36423761+Centupli@users.noreply.github.com>
2019-03-13 13:03:52 -0500

    Added antagonist

---
+++ sourceCode/Scroller/Assets/Scripts/Antagonist/ZombieMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour
+{
+    public PlayerController controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+    public GameObject player;
+    public float pickupDistance;
+
+    private float horizontalMove = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            horizontalMove = horizontalMove * runSpeed;
+            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+
+    private void FixedUpdate()
+    {
+        //for physics
+        controller.Move(-1 * horizontalMove * Time.fixedDeltaTime, false, false);
+        
+    }
+
+    
+}
