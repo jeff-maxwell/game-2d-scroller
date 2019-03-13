346a401ac8dfbc9e58a654613fcf9fc9f20d8d30
Centupli <36423761+Centupli@users.noreply.github.com>
2019-03-13 10:26:13 -0500

    Added player movement, animation, jumping

---
+++ sourceCode/Scroller/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerController controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetKeyDown(KeyCode.Space)) //maybe add key for controller.
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) //maybe add key for controller.
+        {
+            crouch = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            crouch = false;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        //for physics
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+}
