ea999ed6aaa62d0b3efcea0b0a7a41bb6065a5cb
Gabriel Alvarado <gabealvarado652@gmail.com>
2019-03-13 04:02:25 -0500

    Corrected Jump.cs and added new character to JungleScene

--- sourceCode/Scroller/Assets/Scripts/Jump.cs
+++ sourceCode/Scroller/Assets/Scripts/Jump.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    public float xVelocity = 20.0f;
+    public float xVelocity = 22.0f;
     public float yVelocity = 40.0f;
 
     private Rigidbody2D rigidBody;
-    private BoxCollider2D boxCollider;
+    private CapsuleCollider2D capCollider;
     private bool facingRight = true;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        capCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -27,8 +27,8 @@ public class Jump : MonoBehaviour
             //Vertical upward player movement.
             //Get bounds of player's collision box.
             //Look for overlapping colliders in area of same width below player.
-            Vector3 max = boxCollider.bounds.max;
-            Vector3 min = boxCollider.bounds.min;
+            Vector3 max = capCollider.bounds.max;
+            Vector3 min = capCollider.bounds.min;
             Vector2 corner1 = new Vector2(max.x, min.y - .1f);
             Vector2 corner2 = new Vector2(min.x, min.y - .2f);
             Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
@@ -56,6 +56,15 @@ public class Jump : MonoBehaviour
                 rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f);
                 rigidBody.AddForce(new Vector2(xVelocity, yVelocity), ForceMode2D.Impulse);
 
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX = false;
             }
         }
     }
